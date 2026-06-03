cbcca56e928c65f680aaf232a93f3beeb0742dc2
Shivani123-test <shivanisahu2403@gmail.com>
2026-06-03 12:29:39 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,9 @@
+//let score ="33"
+
+//console.log(typeof score);
+//let valueinNumber= Number(score);
+//console.log(typeof valueInNumber);
+let isLoggedIn=1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);

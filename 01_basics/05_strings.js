13f433911e2b3a335a1f68c1fb3bf8aa2627a5fd
Shivani123-test <shivanisahu2403@gmail.com>
2026-06-04 13:00:17 +0000

    nums_math_and_strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const name = "shivani"
+const repoCount = 50
+//console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('abrakadabra')
+console.log(gameName[0]);
+console.log(gameName.charAt[3]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.indexOf('k'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,4 @@
+const score = 400
+console.log(score);
+const happy = new Number(3)
+console.log(happy);

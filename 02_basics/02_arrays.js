337e1ea4cf4b934702dded14505c4e0e24d6dabf
Shivani123-test <shivanisahu2403@gmail.com>
2026-06-09 07:11:22 +0000

    Arrays_basics

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,6 @@
+const marvel_heros = ["thor", "captainA", "Ironman"]
+const dc_heroes = ["batman", "superman", "flash"]
+//marvel_heros.push(dc_heroes)
+//console.log(marvel_heros);
+const allHeroes= marvel_heros.concat(dc_heroes)
+console.log(allHeroes);

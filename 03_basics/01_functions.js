a653c087462dcbf4742ee3f14f0fe72c01faf624
Shivani123-test <shivanisahu2403@gmail.com>
2026-06-11 11:41:48 +0000

    functions_basic

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function myName(){
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("V");
+    console.log("A");
+    console.log("N");
+    console.log("I");
+}
+//myName()
+
+function addTwoNumber(number1, number2){
+    console.log(number1 + number2);
+}
+addTwoNumber(4, 5)
